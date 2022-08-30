ba4246e5a91bb7995f9ebed6ae6a7e5ef2792522
DevRicardoFSantos <ricardofrancosantos@gmail.com>
2022-08-30 18:07:20 -0300

    Calculator with Java Delegate

---
+++ src/main/java/com/JavaDelegate/andExternalTasks/SubtractionDelegate.java
@@ -0,0 +1,15 @@
+package com.JavaDelegate.andExternalTasks;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class SubtractionDelegate implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        Long a =  (Long) execution.getVariable( "a");
+        Long b =  (Long) execution.getVariable( "b");
+
+        execution.setVariable( "result", a - b);
+
+    }
+}
